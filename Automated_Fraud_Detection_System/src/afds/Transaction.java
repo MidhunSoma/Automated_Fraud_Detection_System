9430bd0fbec22245251b68d452dd90731f50cfb2
tejaswar2 <yambaditejaswar2004@gmail.com>
2025-07-03 21:34:48 +0530

    T1

--- Automated_Fraud_Detection_System/src/afds/Transaction.java
+++ Automated_Fraud_Detection_System/src/afds/Transaction.java
@@ -6,4 +6,15 @@ public class Transaction {
 	int riskScore;
 	int accountID;
 	int row,col;
+	
+	public Transaction(int amount,char type,int riskScore, int accountID, int row,int col) {
+		this.amount = amount;
+		this.type = type;
+		this.riskScore = riskScore;
+		this.accountID = accountID;
+		this.row = row;
+		this.col = col;
+		
+		
+	}
 }
