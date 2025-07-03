fde05e1c4da43a717e240d1579772508a8d08bb7
Morahar5462 <kosanamnagapraveenkumar@gmail.com>
2025-07-03 16:48:12 +0530

    updated

--- Automated_Fraud_Detection_System/src/afds/Banking.java
+++ Automated_Fraud_Detection_System/src/afds/Banking.java
@@ -5,7 +5,7 @@ public class Banking {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hii Hello");
+		System.out.println("Hii Morahar");
 	}
 
 }
